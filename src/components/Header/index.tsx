c0bd9eabbd382c31a13a48b60c2f26111d598853
Ava Bakuie <avabakuie@Husseins-MBP-2.fritz.box>
2024-10-07 00:54:19 +0200

    import SearchBar in the Header component, #1

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,10 +1,12 @@
 import { Logo } from "./Logo";
 import { Navbar } from "./Navbar";
+import { SearchBar } from "./Searchbar";
 
 export function Header() {
     return (
         <div className="flex justify-between items-center">
             <Logo />
+            <SearchBar />
             <Navbar />
         </div>
     )
