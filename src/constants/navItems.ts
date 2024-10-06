f2d7fc72ed321f1ed58fa62286f5010a39e2f54f
Ava Bakuie <avabakuie@Husseins-MBP-2.fritz.box>
2024-10-06 23:30:13 +0200

    create anvItems file for navbar items, #1

---
+++ src/constants/navItems.ts
@@ -0,0 +1,14 @@
+export const navItems: { name: string; path: string }[] = [
+    {
+        name: "Likes",
+        path: "/likes",
+    },
+    {
+        name: "Tickets",
+        path: "/tickets",
+    },
+    {
+        name: "login",
+        path: "/login",
+    },
+];
