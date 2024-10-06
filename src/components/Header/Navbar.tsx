d98857b4b7a56300b747ad83d02cd1cc286f38ca
Ava Bakuie <avabakuie@Husseins-MBP-2.fritz.box>
2024-10-06 23:27:46 +0200

    create Navbar component, #1

--- src/components/Header/Navbar.tsx
+++ src/components/Header/Navbar.tsx
@@ -1,1 +1,24 @@
-// nothing to do
+import {
+    NavigationMenu,
+    NavigationMenuItem,
+    NavigationMenuLink,
+} from "@/components/ui/navigation-menu";
+import { navigationMenuTriggerStyle } from "@/components/ui/navigation-menu";
+import { NavLink } from "react-router-dom";
+import { navItems } from "@/constants/navItems";
+
+export function Navbar() {
+    return (
+        <NavigationMenu>
+            <NavigationMenuItem className="flex items-center">
+                {navItems.map((nav) => (
+                    <NavLink to={nav.path}>
+                        <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                            {nav.name}
+                        </NavigationMenuLink>
+                    </NavLink>
+                ))}
+            </NavigationMenuItem>
+        </NavigationMenu>
+    );
+}
