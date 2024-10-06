59affbf21b9b77d0b1c0863d8c3789f99704ff93
Ava Bakuie <avabakuie@Husseins-MBP-2.fritz.box>
2024-10-07 00:53:16 +0200

    create SearchBar component, #1

---
+++ src/components/Header/SearchBar.tsx
@@ -0,0 +1,13 @@
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Search } from 'lucide-react';
+
+export function SearchBar() {
+    return (
+        <div className="flex w-full max-w-lg p-1 rounded-full items-center space-x-2 bg-neutral-100">
+            <Input type="text" placeholder="Search Events" className="flex rounded-none	border-0 border-r border-gray-400 shadow-none focus-visible:ring-0" />
+            <Input type="text" placeholder="Choose a location" className="flex border-0 shadow-none focus-visible:ring-0" />
+            <Button type="submit" variant="secondary" className="flex p-1.5 w-8 h-8 bg-emerald-600 rounded-full border-0 shadow-none focus:outline-none"><Search className="text-white" /></Button>
+        </div>
+    )
+}
