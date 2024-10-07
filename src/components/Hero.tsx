f3478d4273e2a8c00e89b27380a14b4b12cf48ae
Ava Bakuie <avabakuie@Husseins-MBP-2.fritz.box>
2024-10-07 19:59:31 +0200

    create Hero component, #1

---
+++ src/components/Hero.tsx
@@ -0,0 +1,18 @@
+import heroBg from "../assets/images/bg.jpg";
+export function Hero() {
+    return (
+        <div className="flex flex-col rounded-3xl overflow-hidden relative">
+            <div className="w-full flex flex-col items-center justify-center font-bold absolute text-primary text-5xl top-[100px] p-10 z-10">
+                <h1 className="mb-6 text-5xl text-left text-tertiary">
+                    One Click Away from Your Next Adventure!
+                </h1>
+                <span className="text-xl text-left text-tertiary">Experience More – Find the Best Events Here!</span>
+            </div>
+            <img
+                src={heroBg}
+                alt="Background"
+                className="w-full h-[550px] object-cover z-0 brightness-50"
+            />
+        </div>
+    );
+}
